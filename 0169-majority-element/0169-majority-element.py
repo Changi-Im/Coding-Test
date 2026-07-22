a1982b74eae21c77329430b12857984e5ffe7844
Changi Im <imchmcgi2@naver.com>
2026-07-22 21:36:16 +0900

    Time: 4 ms (79.98%), Space: 13.5 MB (73.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,17 +7,4 @@ class Solution(object):
         # sort the array
         nums.sort()
 
-        # count elements using two pointers
-        sp, ep = 0, 0
-        cnt = 0
-        while ep < len(nums):
-            if nums[sp] == nums[ep]:
-                cnt += 1
-            else:
-                sp = ep
-                cnt = 1
-            ep += 1
-
-            if cnt > len(nums) // 2:
-                return nums[sp]
-                
+        return nums[len(nums)//2]
